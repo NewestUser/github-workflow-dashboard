ef01098e9761cb27c3544f03379d50135b51b3f8
Marian Zlatev <mzlatev91@gmail.com>
2022-02-24 19:08:17 +0200

    feature: parse workflow params from logs

--- formatter/html.go
+++ formatter/html.go
@@ -2,6 +2,7 @@ package formatter
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 	"text/template"
 	"time"
@@ -18,7 +19,11 @@ func ToHTML(runs []*github.WorkflowRun) (string, error) {
 func ToHTMLWithCustomLink(runs []*github.WorkflowRun, titleUrlFunc func(*github.WorkflowRun) string) (string, error) {
 	tableRows := &strings.Builder{}
 
-	err := workflowRunHtmlTmpl.Execute(tableRows, &multipleWorkflowRunsDataModel{Workflows: adaptMultipleWorkflowModels(runs, titleUrlFunc)})
+	dataModel := &multipleWorkflowRunsDataModel{
+		Workflows: adaptMultipleWorkflowModels(runs, titleUrlFunc),
+		DisplayParams: containsParams(runs),
+	}
+	err := workflowRunHtmlTmpl.Execute(tableRows, dataModel)
 
 	if err != nil {
 		return "", err
@@ -29,6 +34,7 @@ func ToHTMLWithCustomLink(runs []*github.WorkflowRun, titleUrlFunc func(*github.
 
 type multipleWorkflowRunsDataModel struct {
 	Workflows []*workflowRunModel
+	DisplayParams bool
 }
 
 func adaptMultipleWorkflowModels(runs []*github.WorkflowRun, titleUrlFunc func(*github.WorkflowRun) string) []*workflowRunModel {
@@ -57,6 +63,7 @@ func adaptWorkflowModel(run *github.WorkflowRun, titleUrlFunc func(*github.Workf
 		JobCommitAuthor:  run.JobCommitAuthor,
 		JobCommitMessage: run.JobCommitMessage,
 		JobCommitTime:    timeSince(run.JobCommitTime),
+		JobRunParams:     adaptParams(run.WorkflowParams),
 	}
 }
 
@@ -71,6 +78,31 @@ func truncateStr(value interface{}, size int) string {
 	}
 
 	return v[0:size]
+}
+
+func adaptParams(p *github.WorkflowRunParams) []string {
+	if p == nil {
+		return []string{}
+	}
+
+	setOfParams := map[string]string{}
+	for _, jobParam := range p.Params {
+		for k, v := range jobParam {
+			setOfParams[k] = v
+		}
+	}
+
+	params := make([]string, 0)
+
+	for k, v := range setOfParams {
+		params = append(params, fmt.Sprintf("%s: %s", k, v))
+	}
+
+	sort.Slice(params, func(i, j int) bool {
+		return params[i] > params[j]
+	})
+
+	return params
 }
 
 type workflowRunModel struct {
@@ -90,6 +122,7 @@ type workflowRunModel struct {
 	JobCommitAuthor  string
 	JobCommitMessage string
 	JobCommitTime    string
+	JobRunParams     []string
 }
 
 const workflowRunHtml = `
@@ -100,11 +133,14 @@ const workflowRunHtml = `
 			<th>#</th>
 			<th>Status</th>
 			<th>Branch</th>
-			<th>Author</th>
+			<th>Commiter</th>
 			<th>Commit Msg</th>
 			<th>Commit</th>
 			<th>Commit Time</th>
 			<th>Run Time</th>
+			{{if .DisplayParams}}
+				<th>Params</th>
+			{{end}}
 		</tr>
 	</thead>
 	<tbody>
@@ -123,6 +159,13 @@ const workflowRunHtml = `
 				<td>{{.JobCommitSha}}</td>
 				<td>{{.JobCommitTime}}</td>
 				<td>{{.JobRunTime}}</td>
+				{{if $.DisplayParams}}
+					<td>
+						{{range .JobRunParams}}
+							{{.}}<br/>
+						{{end}}
+					</td>
+				{{end}}
 			</tr>
 		{{end}}
 	</tbody>
---
+++ github/params.go
@@ -0,0 +1,103 @@
+package github
+
+import (
+	"archive/zip"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strings"
+)
+
+// used to narrow down a string that contains env variables
+// example:
+//2022-02-24T11:10:24.8627684Z env:
+//2022-02-24T11:10:24.8628366Z   foo: 1234
+//2022-02-24T11:10:24.8629094Z   bar: xyz
+//2022-02-24T11:10:24.8629861Z ##[endgroup]
+var envGroupRegex = regexp.MustCompile(`(?s)env:.*?\[endgroup\]`)
+
+// used to split a single environment variable new line and extract the key-value pair
+// example:
+//	2022-02-24T11:10:24.8628366Z   foo: 1234
+var envLinePramRegex = regexp.MustCompile(`\s{2,}|\t`)
+
+type inMemoryFile struct {
+	name string
+	body []byte
+}
+
+func parseWorkflowParams(logFiles []*inMemoryFile) ([]JobRunParams, error) {
+	params := make([]JobRunParams, len(logFiles))
+	for i, logFile := range logFiles {
+		runParams, err := parseJobRunLog(logFile)
+		if err != nil {
+			return nil, err
+		}
+		params[i] = runParams
+	}
+
+	return params, nil
+}
+
+func parseJobRunLog(logFile *inMemoryFile) (JobRunParams, error) {
+	result := JobRunParams{}
+
+	envGroups := envGroupRegex.FindAllString(string(logFile.body), -1)
+	if envGroups == nil {
+		return result, nil
+	}
+
+	for _, envGroup := range envGroups {
+		newLines := strings.Split(strings.ReplaceAll(envGroup, "\r\n", "\n"), "\n")
+
+		for i := 0; i < len(newLines); i++ {
+			envLine := newLines[i]
+			if strings.HasPrefix(envLine, "env:") || strings.HasSuffix(envLine, "[endgroup]") {
+				continue
+			}
+
+			parts := envLinePramRegex.Split(envLine, 2)
+			if len(parts) != 2 {
+				return nil, fmt.Errorf("can't extract env variable from log with name %s and log line '%s' since it containts %d parts", logFile.name, envLine, len(parts))
+			}
+
+			keyAndValue := strings.SplitN(parts[1], ": ", 2)
+			if len(keyAndValue) != 2 {
+				return nil, fmt.Errorf("can't extract key-value pair from log with name %s and key-value string '%s' since it contains %d parts", logFile.name, parts[1], len(keyAndValue))
+			}
+			result[keyAndValue[0]] = keyAndValue[1]
+		}
+	}
+	return result, nil
+}
+
+func readZip(buff bytes.Buffer) ([]*inMemoryFile, error) {
+	zipReader, err := zip.NewReader(bytes.NewReader(buff.Bytes()), int64(buff.Len()))
+
+	if err != nil {
+		return nil, err
+	}
+
+	files := make([]*inMemoryFile, len(zipReader.File))
+
+	for i, zipFile := range zipReader.File {
+		zipBytes, err := readZipFile(zipFile)
+		if err != nil {
+			return nil, fmt.Errorf("failed reading zip file %s, err: %s", zipFile.Name, err)
+		}
+
+		files[i] = &inMemoryFile{name: zipFile.Name, body: zipBytes}
+	}
+
+	return files, nil
+}
+
+func readZipFile(zf *zip.File) ([]byte, error) {
+	f, err := zf.Open()
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	return ioutil.ReadAll(f)
+}
